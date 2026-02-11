b122ad528709b8b6d30095368d09c3918db2ca41
Jake Lawrence <jakelawrence.dev@gmail.com>
2026-02-11 15:38:12 -0800

    Remove fixed positioning from Navbar component

--- src/app/components/Navbar.js
+++ src/app/components/Navbar.js
@@ -39,7 +39,7 @@ export default function Navbar({ isLoaded }) {
   };
 
   return (
-    <nav className={`fixed top-0 left-0 right-0 z-[100] bg-fadedBlack transition-all duration-500 ${isLoaded ? "opacity-100 " : "opacity-0"}`}>
+    <nav className={`z-[100] bg-fadedBlack transition-all duration-500 ${isLoaded ? "opacity-100 " : "opacity-0"}`}>
       <div className="max-w-7xl mx-auto px-4 py-2 sm:px-6 lg:px-8">
         <div className="flex items-center justify-between h-16">
           {/* Logo / Brand */}
