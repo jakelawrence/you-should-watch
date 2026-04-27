c3e75d48f111531df5b2aa95d7fa9e4e068fe9a4
jakelawrence <jakelawrence.dev@gmail.com>
2026-04-26 22:02:24 -0700

    fix missing local fonts in build

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,26 +1,7 @@
-import localFont from "next/font/local";
 import "./globals.css";
 import { MovieCollectionProvider } from "./context/MovieCollectionContext";
 import Footer from "./components/Footer";
 import AuthProvider from "./components/AuthProvider";
-
-const dmSerifDisplay = localFont({
-  src: "../../design/fonts/dm-serif-display-v17-latin-regular.woff2",
-  variable: "--font-dm-serif-display",
-  display: "swap",
-});
-
-const bigShouldersDisplay = localFont({
-  src: "../../design/fonts/big-shoulders-v4-latin-regular.woff2",
-  variable: "--font-big-shoulders-display",
-  display: "swap",
-});
-
-const dmSans = localFont({
-  src: "../../design/fonts/dm-sans-v17-latin-regular.woff2",
-  variable: "--font-dm-sans",
-  display: "swap",
-});
 
 export const metadata = {
   title: "You Should Watch",
@@ -45,11 +26,8 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html
-      lang="en"
-      className={`${dmSerifDisplay.variable} ${bigShouldersDisplay.variable} ${dmSans.variable}`}
-    >
-      <body className={`${dmSans.className} antialiased bg-background text-fadedBlack`}>
+    <html lang="en">
+      <body className="antialiased bg-background text-fadedBlack">
         <AuthProvider>
           <MovieCollectionProvider>
             <div>{children}</div>
